c69be8335585c0686b0f9f0755c018ba652f65eb
Filipe <lipeqa@gmail>
2022-07-14 21:20:40 -0300

    feat: adiciona o verbo posto para usuari e tarefas

--- src/controllers/Tarefas.js
+++ src/controllers/Tarefas.js
@@ -3,9 +3,13 @@ import TarefaModel from "../models/TarefaModel.js"
 class Tarefas{
     static rotas(app){
     app.get("/tarefas" ,(req, res) => {
-
         const tarefa = new TarefaModel("Estudar Flutter", "entender a diferença entre stateless e statefull", "1 dia", "alta")
-        res.send(tarefa)
-    })}
+        res.status(200).json({...tarefa, verbo: "get"})
+        })
+    app.post("/tarefas" ,(req, res) => {
+        const tarefa = new TarefaModel("Estudar Flutter", "entender a diferença entre stateless e statefull", "1 dia", "alta")
+        res.status(201).json({...tarefa, verbo: "post"})
+        })
+    }
 }
 export default Tarefas
